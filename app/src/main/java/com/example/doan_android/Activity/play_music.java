bb848525e4d5f5c264cf186d59977a448fcb2b0b
Tuan Anh Dong <neodev146@outlook.com>
2021-01-07 21:32:03 +0700

    - Chưa chơi được nhạc

--- app/src/main/java/com/example/doan_android/Activity/play_music.java
+++ app/src/main/java/com/example/doan_android/Activity/play_music.java
@@ -10,14 +10,18 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
 import com.example.doan_android.Adapter.AdapterPlayerMusic;
+import com.example.doan_android.Adapter.AdapterSong;
+import com.example.doan_android.Fragment.music_player;
 import com.example.doan_android.Model.Album;
 import com.example.doan_android.Model.Baihat;
 import com.example.doan_android.Model.Banner;
@@ -27,6 +31,7 @@ import com.google.android.material.tabs.TabLayout;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 public class play_music extends AppCompatActivity {
 
@@ -34,37 +39,61 @@ public class play_music extends AppCompatActivity {
     //Add view
     private TabLayout tabPlayerMusic;
     private ViewPager view_pager_music;
-    Baihat baihat;
+    public static ArrayList<Baihat> baihatArrayList = new ArrayList<>();
     //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    AdapterSong adapterSong;
+    AdapterPlayerMusic adapterPlayerMusic;
 
-    TextView txtMusicTenBaiHat, txtMusicTenNgheSi, txtMusic, txtTimeSong, txtTotalTimeSong;
+    Baihat baihat;
+
+    TextView txtMusicTenBaiHat, txtMusicTenNgheSi, txtTimeSong, txtTotalTimeSong;
     SeekBar barTime;
-    ImageButton btnPlayShuffle, btnPlayPre, btnPlay, btnPlayNext, btnPlayLoop;
+    ImageView btnPlayShuffle, btnPlayPre, btnPlay, btnPlayNext, btnPlayLoop;
     MediaPlayer mediaPlayer;
+    music_player musicplayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_music);
 
+        //Init
         tabPlayerMusic = findViewById(R.id.tabPlayerMusic);
         view_pager_music = findViewById(R.id.view_pager_music);
-
+        txtMusicTenBaiHat = findViewById(R.id.txtMusicTenBaiHat);
+        txtMusicTenNgheSi = findViewById(R.id.txtbtmTenNgheSi);
+        txtTimeSong = findViewById(R.id.txtTimeSong);
+        txtTotalTimeSong = findViewById(R.id.txtTotalTimeSong);
+        btnPlay = findViewById(R.id.btnPlay);
+        btnPlayShuffle = findViewById(R.id.btnPlayShuffle);
+        btnPlayPre = findViewById(R.id.btnPlayPre);
+        btnPlayNext = findViewById(R.id.btnPlayNext);
+        btnPlayLoop = findViewById(R.id.btnPlayLoop);
         //ViewPager for display fragments
         AdapterPlayerMusic adaptermusic = new AdapterPlayerMusic(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
         view_pager_music.setAdapter(adaptermusic);
         tabPlayerMusic.setupWithViewPager(view_pager_music);
+        musicplayer = (music_player) adapterPlayerMusic.getItem(0);
+
+        if (baihatArrayList.size() > 0){
+            new PlayMp3File().execute(baihatArrayList.get(0).getUrlBaihat());
+            btnPlay.setImageResource(R.drawable.ic_baseline_pause_24);
+        }
 
         //Set icon
         tabPlayerMusic.getTabAt(0).setIcon(R.drawable.ic_baseline_list_alt_24);
         tabPlayerMusic.getTabAt(1).setIcon(R.drawable.ic_baseline_music_note_24).select();
         tabPlayerMusic.getTabAt(2).setIcon(R.drawable.ic_baseline_text_fields_24);
+
+        GetDataFromItent();
+
+        EventClick();
+
         Getdata();
 
     }
 
-
-    class PlayMp3 extends AsyncTask<String, Void, String> {
+  class PlayMp3File extends AsyncTask<String, Void, String> {
 
         @Override
         protected String doInBackground(String... strings) {
@@ -109,4 +138,53 @@ public class play_music extends AppCompatActivity {
             }
         }
     }
+
+    private void EventClick(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+          @Override
+          public void run() {
+              if (adapterPlayerMusic.getItem(1) != null){
+                if(baihatArrayList.size() > 0){
+                    musicplayer.Playnhac(baihatArrayList.get(0).getHinhBaihat());
+                    handler.removeCallbacks(this);
+                }
+                else{
+                      handler.postDelayed(this, 300);
+                }
+              }
+          }
+        }, 500);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              if(mediaPlayer.isPlaying()){
+                mediaPlayer.pause();
+                btnPlay.setImageResource(R.drawable.ic_baseline_play_arrow_24);
+              }
+              else
+              {
+                mediaPlayer.start();
+                btnPlay.setImageResource(R.drawable.ic_baseline_pause_24);
+              }
+          }
+        });
+    }
+
+    private void GetDataFromItent() {
+      Intent intent = getIntent();
+      baihatArrayList.clear();
+      if(intent != null){
+        if(intent.hasExtra("cakhuc")){
+          Baihat baihat = intent.getParcelableExtra("cakhuc");
+          baihatArrayList.add(baihat);
+        }
+        if(intent.hasExtra("cacbaihat")){
+          ArrayList<Baihat> baihatArr = intent.getParcelableExtra("cacbaihat");
+          baihatArrayList = baihatArr;
+        }
+      }
+    }
+
+
 }
--- app/src/main/java/com/example/doan_android/Fragment/music_player.java
+++ app/src/main/java/com/example/doan_android/Fragment/music_player.java
@@ -1,11 +1,14 @@
 package com.example.doan_android.Fragment;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
 import android.os.Handler;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,147 +18,35 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.doan_android.Adapter.AdapterSong;
+import com.example.doan_android.Model.Baihat;
 import com.example.doan_android.R;
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 
 public class music_player extends Fragment {
 
       public music_player(){
-
+        // Required empty public constructor
       }
 
       View view;
       ImageView imgMusicPlayer;
-      TextView txtTimeSong, txtTotalTimeSong;
-      SeekBar barTime;
-      MediaPlayer mediaPlayer;
-      Handler handler = new Handler();
-      ImageView btnPre, btnPlay, btnNext;
-
-//    // TODO: Rename parameter arguments, choose names that match
-//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//    private static final String ARG_PARAM1 = "param1";
-//    private static final String ARG_PARAM2 = "param2";
-//
-//    // TODO: Rename and change types of parameters
-//    private String mParam1;
-//    private String mParam2;
-//
-//    public music_player() {
-//        // Required empty public constructor
-//    }
-//
-//    /**
-//     * Use this factory method to create a new instance of
-//     * this fragment using the provided parameters.
-//     *
-//     * @param param1 Parameter 1.
-//     * @param param2 Parameter 2.
-//     * @return A new instance of fragment music_player.
-//     */
-//    // TODO: Rename and change types and number of parameters
-//    public static music_player newInstance(String param1, String param2) {
-//        music_player fragment = new music_player();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
-//
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//
-//        }
-//    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_music_player, container, false);
-//        imgMusicPlayer = view.findViewById(R.id.imgMusicPlayer);
-//        btnPlay = view.findViewById(R.id.btnPlay);
-//        imgMusicPlayer = view.findViewById(R.id.imgMusicPlayer);
-//        txtTimeSong = view.findViewById(R.id.txtTimeSong);
-//        txtTotalTimeSong = view.findViewById(R.id.txtTotalTimeSong);
-//        barTime = view.findViewById(R.id.barTime);
-//        mediaPlayer = new MediaPlayer();
-//        barTime.setMax(100);
-//
-//        btnPlay.setOnClickListener(new View.OnClickListener() {
-//          @Override
-//          public void onClick(View view) {
-//            if(mediaPlayer.isPlaying()){
-//              handler.removeCallbacks(updater);
-//              mediaPlayer.pause();
-//              btnPlay.setImageResource(R.drawable.ic_baseline_play_arrow_24);
-//            }
-//            else
-//            {
-//              mediaPlayer.start();
-//              btnPlay.setImageResource(R.drawable.ic_baseline_pause_24);
-//              updateTimeBar();
-//            }
-//          }
-//        });
-//
-//        linkforMediaPlayer();
-//        //return inflater.inflate(R.layout.fragment_music_player, container, false);
-          return  view;
+        imgMusicPlayer = view.findViewById(R.id.imgMusicPlayer);
+        return  view;
     }
 
-//    private void linkforMediaPlayer(){
-//      try {
-//        mediaPlayer.setDataSource("https://tranquochung1711061818.000webhostapp.com/nhac/NeuMaiNayXaNhau-HoaMinzy-6891719.mp3");
-//        mediaPlayer.prepare();
-//        txtTotalTimeSong.setText(secondToTimer(mediaPlayer.getDuration()));
-//      }
-//      catch (Exception exception){
-//
-//      }
-//    }
-//
-//    private Runnable updater = new Runnable() {
-//      @Override
-//      public void run() {
-//        updateTimeBar();
-//        long currentDuration = mediaPlayer.getCurrentPosition();
-//        txtTimeSong.setText(secondToTimer(currentDuration));
-//      }
-//    };
-//
-//    private void updateTimeBar(){
-//      if(mediaPlayer.isPlaying()){
-//        barTime.setProgress((int) (((float) mediaPlayer.getCurrentPosition() / mediaPlayer.getDuration()) * 100));
-//        handler.postDelayed(updater, 1000);
-//      }
-//    }
-//
-//    private String secondToTimer(long second){
-//        String timerString = "";
-//        String secondString;
-//
-//        int h = (int) (second / (1000 * 60 * 60));
-//        int m = (int) (second % (1000 * 60 * 60)) / (1000 * 60);
-//        int s = (int) ((second % (1000 * 60 * 60)) % (1000 * 60) / 1000);
-//
-//        if (h > 0){
-//          timerString = h + ":";
-//        }
-//
-//        if (s < 10){
-//          secondString = "0" + s;
-//        }
-//        else
-//        {
-//          secondString = "" + s;
-//        }
-//
-//        timerString = timerString + m + ":" + secondString;
-//        return timerString;
-//    }
+    public void Playnhac(String txtimgMusicPlayer) {
+      Picasso.get().load(txtimgMusicPlayer).into(imgMusicPlayer);
+    }
+
 }
