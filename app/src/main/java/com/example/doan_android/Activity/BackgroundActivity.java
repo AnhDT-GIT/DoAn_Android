e58c840dd98e822a1f91083682f59e4cf74db156
Tuyen <Nguyenthikimtuyeen@gmail.com>
2021-01-10 15:05:54 +0700

    xu ly

--- app/src/main/java/com/example/doan_android/Activity/BackgroundActivity.java
+++ app/src/main/java/com/example/doan_android/Activity/BackgroundActivity.java
@@ -5,15 +5,18 @@ import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
+import android.media.Image;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.example.doan_android.Adapter.AdapterUI;
 import com.example.doan_android.Model.Song;
 import com.example.doan_android.R;
 import com.google.android.material.tabs.TabLayout;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -29,8 +32,7 @@ public class BackgroundActivity extends AppCompatActivity {
     public static ArrayList<Song> listSong = new ArrayList<>();
     public static int position = 0;
     public static int check = 0;
-
-
+    ImageView image;
     //private LinearLayout layoutThongTin;
 
     @Override
@@ -43,7 +45,7 @@ public class BackgroundActivity extends AppCompatActivity {
         tabMain = findViewById(R.id.tabMain);
         viewPagerMain = findViewById(R.id.view_pager);
 
-
+        image =findViewById(R.id.imgbottomPlay);
         //ViewPager for display fragments
         AdapterUI adapterUI = new AdapterUI(getSupportFragmentManager(),
                 FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
@@ -61,6 +63,61 @@ public class BackgroundActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 startActivityPlayMusic();
+            }
+        });
+        findViewById(R.id.imgbottomPlay).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (BackgroundActivity.mediaPlayer.isPlaying()) {
+                    BackgroundActivity.mediaPlayer.pause();
+                    image.setImageResource(R.drawable.ic_baseline_play_arrow_24);
+                } else {
+                    BackgroundActivity.mediaPlayer.start();
+                    image.setImageResource(R.drawable.ic_baseline_pause_24);
+                }
+            }
+        });
+        findViewById(R.id.imgbottomNext).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (BackgroundActivity.listSong.size() > 0) {
+                    if (BackgroundActivity.mediaPlayer.isPlaying() || BackgroundActivity.mediaPlayer != null) {
+                        BackgroundActivity.mediaPlayer.stop();
+                        BackgroundActivity.mediaPlayer.release();
+                        BackgroundActivity.mediaPlayer = null;
+                    }
+                    if (BackgroundActivity.position < (BackgroundActivity.listSong.size())) {
+                        image.setImageResource(R.drawable.ic_baseline_pause_24);
+                        BackgroundActivity.position++;
+                        if (BackgroundActivity.position > (BackgroundActivity.listSong.size() - 1)) {
+                            BackgroundActivity.position = 0;
+                        }
+                        new PlayMusicActivity.PlayMp3().execute(BackgroundActivity.listSong.get(BackgroundActivity.position).getUrlBaihat());
+                    }
+                }
+
+            }
+        });
+        findViewById(R.id.imgbottomPre).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (BackgroundActivity.listSong.size() > 0) {
+                    if (BackgroundActivity.mediaPlayer.isPlaying() || BackgroundActivity.mediaPlayer != null) {
+                        BackgroundActivity.mediaPlayer.stop();
+                        BackgroundActivity.mediaPlayer.release();
+                        BackgroundActivity.mediaPlayer = null;
+                    }
+                    if (BackgroundActivity.position < (BackgroundActivity.listSong.size())) {
+                        image.setImageResource(R.drawable.ic_baseline_pause_24);
+                        BackgroundActivity.position--;
+
+                        if (BackgroundActivity.position < 0) {
+                            BackgroundActivity.position = BackgroundActivity.listSong.size() - 1;
+                        }
+
+                        new PlayMusicActivity.PlayMp3().execute(BackgroundActivity.listSong.get(BackgroundActivity.position).getUrlBaihat());
+                    }
+                }
             }
         });
     }
